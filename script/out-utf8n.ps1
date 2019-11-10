05b707c8de89ff7adb05c26c5156a62312de583e
Sakai Yusuke <sakai.iplab@gmail.com>
2019-11-11 00:20:51 +0900

    [add] output file in utf8 with/without bom

---
+++ script/out-utf8n.ps1
@@ -0,0 +1,33 @@
+function Out-UTF8 {
+    param (
+        [Parameter(Mandatory=$True, ValueFromPipeline=$true)]
+        [Array] $line,
+        [Parameter(Mandatory=$True)]
+        [String] $File,
+        [Switch] $CrLf,
+        [Switch] $BOM
+    )
+
+    begin {
+        $EOL = "`n"
+        if ($CRLF) {
+            $EOL = "`r`n"
+        }
+        $lines = @()
+    }
+
+    process {
+        $lines += $line
+    }
+
+    end {
+        Write-Host $File
+        if ($BOM) {
+            $lines -join $EOL | Out-File -Encoding UTF8 $File -NoNewline
+        } else {
+            $lines -join $EOL `
+            | % { [Text.Encoding]::UTF8.GetBytes($_) } `
+            | Set-Content -Path $File -Encoding Byte
+        }
+    }
+}
