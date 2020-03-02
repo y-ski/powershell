b02bbfda76b5215e84b612601a265c2cdbd18c34
Sakai Yusuke <sakai.iplab@gmail.com>
2020-03-03 00:58:23 +0900

    [add] head

---
+++ script/head.ps1
@@ -0,0 +1,17 @@
+function head {
+    param (
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $path,
+        [Parameter()]
+        [int] $n = 10,
+    )
+    begin {}
+    process {
+        if (Test-Path $path) {
+            Get-Content $path -Head $n
+        } else {
+            Write-Host "not found: $path"
+        }
+    }
+    end {}
+}
