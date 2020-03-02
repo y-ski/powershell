4976877c6ff9f5446e55e0a542e4b196fc37e6b1
Sakai Yusuke <sakai.iplab@gmail.com>
2020-03-03 00:58:12 +0900

    [add] tail

---
+++ script/tail.ps1
@@ -0,0 +1,22 @@
+function tail {
+    param (
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $path,
+        [Parameter()]
+        [int] $n = 10,
+        [switch] $f
+    )
+    begin {}
+    process {
+        if (Test-Path $path) {
+            if ($f) {
+                Get-Content $path -Tail $n -Wait
+            } else {
+                Get-Content $path -Tail $n
+            }
+        } else {
+            Write-Host "not found: $path"
+        }
+    }
+    end {}
+}
